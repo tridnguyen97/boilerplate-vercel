23c65411387c1bc1b6279df436c31f8b7c1d51a3
tridn <tridnguyen97@gmail.com>
2023-10-04 18:56:47 +0700

    remove redundance

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -1,5 +1,5 @@
 const Joi = require('joi');
-const { password, objectId } = require('./custom.validation');
+const { password } = require('./custom.validation');
 
 const createUser = {
   body: Joi.object().keys({
